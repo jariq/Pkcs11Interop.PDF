567d56546323a41349b8bd50b5d24ebc34b99373
Jaroslav Imrich <jaroslav.imrich@gmail.com>
2014-05-08 10:04:38 +0200

    Added empty demo application

---
+++ src/Pkcs11Interop.PDF.Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ *  Pkcs11Interop.PDF.Demo - Demonstration application
+ *                           for Pkcs11Interop.PDF library
+ *  Copyright (c) 2013-2014 JWC s.r.o. <http://www.jwc.sk>
+ *  Author: Jaroslav Imrich <jimrich@jimrich.sk>
+ *
+ *  Licensing for open source projects:
+ *  Pkcs11Interop.PDF is available under the terms of the GNU Affero General 
+ *  Public License version 3 as published by the Free Software Foundation.
+ *  Please see <http://www.gnu.org/licenses/agpl-3.0.html> for more details.
+ *
+ *  Licensing for other types of projects:
+ *  Pkcs11Interop.PDF is available under the terms of flexible commercial license.
+ *  Please contact JWC s.r.o. at <info@pkcs11interop.net> for more details.
+ */
+
+using System;
+
+namespace Net.Pkcs11Interop.PDF.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ src/Pkcs11Interop.PDF.Demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pkcs11Interop.PDF.Demo")]
+[assembly: AssemblyDescription("Demonstration application for Pkcs11Interop.PDF library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JWC s.r.o.")]
+[assembly: AssemblyProduct("Pkcs11Interop.PDF (AGPL)")]
+[assembly: AssemblyCopyright("Copyright © 2013-2014 JWC s.r.o. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("55a54cdb-b258-4eb6-b245-f6b68dfe57ff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
